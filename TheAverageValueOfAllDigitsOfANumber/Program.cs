1916b281d24b4f0bdf0345ac11025972d4d72a5c
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2025-03-19 18:18:39 +0300

    Add files via upload

---
+++ TheAverageValueOfAllDigitsOfANumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Lütfen basamaklarının ortalaması bulunacak sayıyı giriniz: ");
+int number = int.Parse(Console.ReadLine());
+int originalNum = number;
+
+double result = 0;
+int digit = 0;
+
+while (number > 0) {
+    result += number % 10;
+    number /= 10;
+    digit++;
+}
+
+result = result / digit;
+
+Console.WriteLine(originalNum + " sayısının basamaklarının ortalaması: " + result);
