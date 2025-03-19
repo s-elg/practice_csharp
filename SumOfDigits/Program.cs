a0fe1ad187c625f82c2a18c30ff9df66f3395b9b
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2025-03-19 18:13:29 +0300

    project of sum of digits has been created

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Lütfen basamaklarının toplamı bulunacak sayıyı giriniz: ");
+int number = int.Parse(Console.ReadLine());
+int originalNum = number;
+
+int result = 0;
+
+while (number > 0) {
+    result += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine(originalNum + " sayısının basamaklarının toplamı: " + result);
