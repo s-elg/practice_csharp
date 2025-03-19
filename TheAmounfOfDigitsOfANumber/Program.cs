a7321a0cf8f1b064bb7718816317bf3cc61ca710
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2025-03-19 18:42:27 +0300

    Update Program.cs

--- TheAmounfOfDigitsOfANumber/Program.cs
+++ TheAmounfOfDigitsOfANumber/Program.cs
@@ -1,1 +1,12 @@
+Console.Write("Lütfen basamak sayısı bulunacak sayıyı giriniz: ");
+int number = int.Parse(Console.ReadLine());
+int originalNum = number;
 
+int digit = 0; 
+
+while (number > 0) {
+    digit++;
+    number /= 10;
+}
+
+Console.WriteLine(originalNum + " sayısının basamaklarının sayısı: " + digit);
