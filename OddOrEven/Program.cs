df77055424a0bac50dd8eb6878be7c537bd92c54
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2025-03-19 18:00:00 +0300

    odd or even project created

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Odd or Even");
+Console.Write("Please enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+string result = number % 2 == 0 ? "even" : "odd";
+
+Console.WriteLine(number + " is an " + result + " number.");
